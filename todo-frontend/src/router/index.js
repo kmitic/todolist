2f49c20814ecdf08d3a5a88620c626dbc5c15d8d
Katarina Mitic <katarina.mitic21@gmail.com>
2019-12-11 12:01:01 +0100

    prve vezbe

--- todo-frontend/src/router/index.js
+++ todo-frontend/src/router/index.js
@@ -1,30 +1,14 @@
 import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vuex from "vuex";
 
-Vue.use(VueRouter);
+Vue.use(Vuex);
 
-const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
-];
-
-const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes
+export default new Vuex.Store({
+  state: {
+    baseUrl: 'http://localhost/todo'
+  },	
+  mutations: {},
+  actions: {},
+  modules: {}
 });
 
-export default router;
